0d9319ce03e641f01d09496a87f5a060eba5759c
Amir Abu Shareb <yields@icloud.com>
2014-03-26 17:21:40 +0200

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,18 @@
+
+# highlight-ruby
+
+  Highlight plugin for Ruby
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install segmentio/highlight-ruby
+
+## API
+
+
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,73 @@
+
+/**
+ * Expose `ruby`
+ */
+
+module.exports = ruby;
+
+/**
+ * Add `ruby` as a language.
+ *
+ * @param {Highlight} highlight
+ * @api public
+ */
+
+function ruby(highlight){
+  highlight.language('ruby', ruby);
+}
+
+/**
+ * Booleans
+ */
+
+ruby.boolean = /\b(true|false)\b/;
+
+/**
+ * Comments
+ */
+
+ruby.comment = /(?!\\{2})(#.*?$)/m;
+
+/**
+ * Classes
+ */
+
+ruby.class = /class +(\w+)/;
+ruby.class.children = { keyword: /class/ };
+
+/**
+ * Keywords
+ */
+
+ruby.keyword = /\b(alias|and|begin|break|case|catch|class|def|do|elsif|else|fail|ensure|for|end|if|in|module|next|not|or|raise|redo|rescue|retry|return|then|throw|super|unless|undef|until|when|while|yield)\b/;
+
+/**
+ * Numbers
+ */
+
+ruby.number = /\b[-+]?(0x[a-fA-F0-9]+|0b[0-1_]+|0[0-7]*|[1-9][0-9_eE.]*)\b/;
+
+/**
+ * Strings
+ */
+
+ruby.string = /(("|')(\\?.)*?\2)/;
+
+/**
+ * Functions
+ */
+
+ruby.function = /(\w+) *\(/;
+ruby.function.children = { punctuation: /\(/ };
+
+/**
+ * Operator
+ */
+
+ruby.operator = /([-+]{1,2}|!|&lt;=?|>=?|={1,3}|&lt;{1,2}|>{1,2}|(&amp;){1,2}|\|{1,2}|\?|\*|\/|\~|\^|\%)/;
+
+/**
+ * Punctuation
+ */
+
+ruby.punctuation = /[{}[\];(),.:]/;
